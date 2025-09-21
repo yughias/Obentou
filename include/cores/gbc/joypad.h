25983044e82286044bfd0a2dc278d99e2aa691d7
yughias <sasa.laporta@gmail.com>
2025-09-21 12:32:46 +0200

    removed macros

--- include/cores/gbc/joypad.h
+++ include/cores/gbc/joypad.h
@@ -5,21 +5,6 @@
 
 #include "types.h"
 
-#define AXIS_DEAD_ZONE (1 << 13)
-#define GAMECONTROLLER_CHECK(btn) (gameController && SDL_GameControllerGetButton(gameController, SDL_CONTROLLER_BUTTON_ ## btn)) 
-#define NORM_AXIS(axis) if(axis > AXIS_DEAD_ZONE) axis = 1; else if(axis < -AXIS_DEAD_ZONE) axis = -1; else axis = 0
-
-#define LEFT_KEY SDL_SCANCODE_LEFT
-#define RIGHT_KEY SDL_SCANCODE_RIGHT
-#define UP_KEY SDL_SCANCODE_UP
-#define DOWN_KEY SDL_SCANCODE_DOWN
-#define A_KEY SDL_SCANCODE_X
-#define B_KEY SDL_SCANCODE_Z
-#define SELECT_KEY SDL_SCANCODE_RSHIFT
-#define START_KEY SDL_SCANCODE_RETURN
-#define TURBO_A_KEY SDL_SCANCODE_S 
-#define TURBO_B_KEY SDL_SCANCODE_A
-
 #define JOYP_ADDR 0xFF00
 
 typedef struct joypad_t {
