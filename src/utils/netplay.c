028805a36182b8c59a5cebb1442eaa9e6e6356fd
yughias <sasa.laporta@gmail.com>
2026-05-01 14:59:49 +0200

    fixed netplay stub for emscripten

--- src/utils/netplay.c
+++ src/utils/netplay.c
@@ -212,7 +212,7 @@ NETPLAY_MODE netplay_actual_mode;
 
 void netplay_init(){}
 void netplay_quit(){}
-void netplay_start_session(){}
+void netplay_start_session(core_ctx_t* ctx){}
 void netplay_close_session(){}
 bool netplay_is_connected(){ return false; }
 void netplay_send_inputs(const core_t* core){}
